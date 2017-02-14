0966cefe537bd70d294e85ab1c9c8cab37d8692f
Alexander Rusakov <a_s_rusakov@mail.ru>
2017-02-14 20:37:36 +0300

    webhook or get updates

--- udoit.go
+++ udoit.go
@@ -7,7 +7,23 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+func getUpdatesChan(bot *tgbotapi.BotAPI) (tgbotapi.UpdatesChannel, error) {
+	webhook := os.Getenv("UDOIT_WEBHOOK") != ""
+
+	if webhook {
+		_, err := bot.SetWebhook(tgbotapi.NewWebhook("")) // todo url
+		if err != nil {
+			log.Panicf("failed to set webhook: %s", err)
+		}
+
+		return bot.ListenForWebhook("/" + bot.Token), nil
+	}
+
+	return bot.GetUpdatesChan(tgbotapi.UpdateConfig{Timeout: 60})
+}
+
 func main() {
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("UDOIT_API_TOKEN"))
 	if err != nil {
 		log.Panicf("failed to init bot api: %s", err)
@@ -16,7 +32,7 @@ func main() {
 	log.Printf("authorized on account %s", bot.Self.UserName)
 	bot.Debug = true
 
-	updates, err := bot.GetUpdatesChan(tgbotapi.UpdateConfig{Timeout: 60})
+	updates, err := getUpdatesChan(bot)
 	if err != nil {
 		log.Panicf("failed to get updates: %s", err)
 	}
