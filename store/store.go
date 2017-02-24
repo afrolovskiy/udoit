01f0d1417ae196150a57a2c6f248eb4a86e51f83
arusakov <a_s_rusakov@mail.ru>
2017-02-25 01:46:47 +0300

    next

--- store/store.go
+++ store/store.go
@@ -14,20 +14,20 @@ type Task struct {
 	CreatorID   int
 }
 
-const sqlCreateSequence = "CREATE SEQUENCE IF NOT EXISTS $1"
+const sqlCreateSequence = "CREATE SEQUENCE IF NOT EXISTS "
 const sqlSelectNextSeqVal = "SELECT nextval($1)"
 const sqlInsertTask = `INSERT INTO tasks (id_in_chat, chat_id, creator_id, description) ` +
 	`VALUES ($1, $2, $3, $4) RETURNING id`
 
 func createSequenceName(chatID int64) string {
-	name := "\"chat_" + fmt.Sprintf("%d", chatID) + "_seq\""
+	name := "chat_" + fmt.Sprintf("%d", chatID) + "_seq"
 	return name
 }
 
 // CreateTask ...
 func CreateTask(db *sql.DB, desc string, creatorID int, chatID int64) (*Task, error) {
 	seqname := createSequenceName(chatID)
-	db.Query(sqlCreateSequence, seqname)
+	db.Query(sqlCreateSequence + "\"" + seqname + "\"")
 
 	t := &Task{Description: desc, CreatorID: creatorID, ChatID: chatID}
 
